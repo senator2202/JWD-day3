b7e68122db03400b74d80a948398167300ac80a2
Alexey <senator220291@gmail.com>
2020-06-12 14:40:46 +0300

    First commit

---
+++ test/by/kharitonov/validator/BasketBallValidatorTest.java
@@ -0,0 +1,12 @@
+package by.kharitonov.validator;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class BasketBallValidatorTest {
+
+    @Test
+    public void testPossibleToPut() {
+    }
+}
